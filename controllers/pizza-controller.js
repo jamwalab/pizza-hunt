870c2bd48ae4f1eb0557faf32d9df164387be277
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-08-17 12:11:05 -0400

    validators added tp pizza model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -50,7 +50,7 @@ const pizzaController = {
   // update pizza by id
   updatePizza({ params, body }, res) {
     //{new: true} makes it retuen the new update value
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators:true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
